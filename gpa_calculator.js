394449e818441a9640f4c711dcdfebe5ee03b009
pixelhalide <taintedxenon@gmail.com>
2025-03-21 17:37:45 +0530

    Gpa calc test

---
+++ gpa_calculator.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("dropdown-button").onclick = function () {
+        document.getElementById("dropdown-arrow").classList.add("rotate-180");
+    };
+});
