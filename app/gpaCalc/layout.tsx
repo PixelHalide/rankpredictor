3c1c3a1fe48f671a7f4a614d44ec15b121e35e3c
PixelHalide <taintedxenon@gmail.com>
2025-08-03 20:57:48 +0530

    SEO

--- app/gpaCalc/layout.tsx
+++ app/gpaCalc/layout.tsx
@@ -3,13 +3,17 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "MIT GPA Calculator",
   description: "Eager to know your MIT GPA? Our GPA calculator allows you to effortlessly predict your GPA, whichever semester it may be.",
+  metadataBase: new URL("https://rankpredictor.in"),
   alternates: {
     canonical: "/gpaCalc",
   },
   openGraph: {
+    type: "website",
+    locale: "en_US",
     title: "MIT GPA Calculator",
     description: "Eager to know your MIT GPA? Our GPA calculator allows you to effortlessly predict your GPA, whichever semester it may be.",
     url: "https://rankpredictor.in/gpaCalc",
+    siteName: "RankPredictor",
     images: [
       {
         url: "/gpa.png",
@@ -20,6 +24,7 @@ export const metadata: Metadata = {
     ],
   },
   twitter: {
+    card: "summary_large_image",
     title: "MIT GPA Calculator",
     description: "Eager to know your MIT GPA? Our GPA calculator allows you to effortlessly predict your GPA, whichever semester it may be.",
     images: ["/gpa.png"],
--- app/met2025/layout.tsx
+++ app/met2025/layout.tsx
@@ -3,16 +3,20 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "MET 2025 Rank Predictor",
   description: "Eager to know where you stand in the MET 2025 rankings? Our advanced MET 2025 Rank Predictor allows you to effortlessly estimate your rank using your MET score. Discover which branches you qualify for, including sought-after fields like CSE, ECE, and many more. Get a head start on your future by understanding your potential academic opportunities today!",
+  metadataBase: new URL("https://rankpredictor.in"),
   alternates: {
     canonical: "/met2025",
   },
   openGraph: {
+    type: "website",
+    locale: "en_US",
     title: "MET 2025 Rank Predictor",
     description: "Eager to know where you stand in the MET 2025 rankings? Our advanced MET 2025 Rank Predictor allows you to effortlessly estimate your rank using your MET score. Discover which branches you qualify for, including sought-after fields like CSE, ECE, and many more. Get a head start on your future by understanding your potential academic opportunities today!",
     url: "https://rankpredictor.in/met2025",
+    siteName: "RankPredictor",
     images: [
       {
-        url: "/rankpredictor.png",
+        url: "/rankPredictor.png",
         width: 1200,
         height: 630,
         alt: "MET 2025 Rank Predictor",
@@ -20,9 +24,10 @@ export const metadata: Metadata = {
     ],
   },
   twitter: {
+    card: "summary_large_image",
     title: "MET 2025 Rank Predictor",
     description: "Eager to know where you stand in the MET 2025 rankings? Our advanced MET 2025 Rank Predictor allows you to effortlessly estimate your rank using your MET score. Discover which branches you qualify for, including sought-after fields like CSE, ECE, and many more. Get a head start on your future by understanding your potential academic opportunities today!",
-    images: ["/rankpredictor.png"],
+    images: ["/rankPredictor.png"],
   },
 };
 
